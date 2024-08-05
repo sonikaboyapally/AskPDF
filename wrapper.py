2a4395ff63009e7a3005bfba463cb3f14754d10e
Sonika Boyapally <sonikaboyapally@Sonikas-Air.attlocal.net>
2024-08-04 23:22:36 -0500

    adding record extraction function calling capability

--- wrapper.py
+++ wrapper.py
@@ -6,7 +6,7 @@ MODEL_NAME = "claude-3-opus-20240229"  # can change model to haiku/sonnet
 
 
 def extract_text_from_pdf(path):
-    reader = PdfReader("/Users/sonikaboyapally/Downloads/fw9.pdf")
+    reader = PdfReader(path)
     number_of_pages = len(reader.pages)
     if number_of_pages > 1:
         pass
@@ -18,17 +18,44 @@ def extract_text_from_pdf(path):
 def get_completion(llm_client, prompt):
     return llm_client.messages.create(
         model=MODEL_NAME,
-        max_tokens=2048,
+        max_tokens=100,
         messages=[{
             "role": 'user', "content": prompt
         }]
     ).content[0].text
 
+def build_summarize_prompt(text):
+    summarize_prompt = f"""Here is an document I am interested in understanding: <paper>{text}</paper>
+    Please do the following:
+    Summarize the abstract at a kindergarten reading level in less than 100 words. (In <kindergarten_abstract> tags.)"""
+    return summarize_prompt
 
-completion = get_completion(client,
-                            f"""Here is an document I am interested in understanding: <paper>{text}</paper>
-                            Please do the following:
-                            1. Summarize the abstract at a kindergarten reading level. (In <kindergarten_abstract> tags.)"""
-                            )
 
-print(completion)
+def build_records_extraction_prompt(text):
+    records_prompt = f'''
+    @dataclass
+    class Record:
+        name : str
+        role : str
+    
+    Function:
+    def identities(names : List[Record]):
+    """
+    given the name and role of the people 
+    """
+    
+    User_Query : given {text}, identify all the records of people in the document and their role<human_end>
+    
+    '''
+    return records_prompt
+
+if __name__ == "__main__":
+    # path = "/Users/sonikaboyapally/Downloads/fw9.pdf"
+    # summarize_prompt = build_summarize_prompt(extract_text_from_pdf(path))
+    # completion = get_completion(client, summarize_prompt)
+    # print(completion)
+    pass
+
+
+
+
