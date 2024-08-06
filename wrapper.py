6c91410a581c4899607e585160480a1e2cd04378
Sonika Boyapally <sonikaboyapally@Sonikas-Air.attlocal.net>
2024-08-05 23:42:53 -0500

    adding app with validations

--- wrapper.py
+++ wrapper.py
@@ -1,9 +1,14 @@
 from pypdf import PdfReader
 from anthropic import Anthropic
 
-client = Anthropic()  # have to load api_key from env file
-MODEL_NAME = "claude-3-opus-20240229"  # can change model to haiku/sonnet
+my_api_key = "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"
+#
+# client = Anthropic()  # have to load api_key from env file
+# MODEL_NAME = "claude-3-opus-20240229"  # can change model to haiku/sonnet
 
+client = Anthropic(api_key = my_api_key)  # have to load api_key from env file
+# MODEL_NAME = "claude-3-opus-20240229"  # can change model to haiku/sonnet
+MODEL_NAME = "claude-3-haiku-20240307"
 
 def check_number_of_pages(pdf_reader):
     # checks number of pages
@@ -70,15 +75,16 @@ def build_general_prompt(text, actual_query):
     return general_prompt
 
 
+def main(given_path, user_prompt):
+    given_text = extract_text_from_pdf(given_path)
+    input_tokens = len(given_text.split(' '))
+    if input_tokens > 4000:
+        return "input tokens exceed 4000"
+    given_prompt = build_general_prompt(given_text, user_prompt)
+    completion = get_completion(client, given_prompt)
+    return completion
 
-if __name__ == "__main__":
-    # path = "/Users/sonikaboyapally/Downloads/fw9.pdf"
-    # given_text = extract_text_from_pdf(path)
-    # user_question = " "
-    # general_prompt = build_summarize_prompt(given_text, user_question)
-    # completion = get_completion(client, general_prompt)
-    # print(completion)
-    pass
+
 
 
 
